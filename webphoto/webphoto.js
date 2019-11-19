4a3f79d2a1fd36601b844a212b74c85575f791b6
Ravbug <ravbuganimations@gmail.com>
2019-11-19 15:16:10 -0500

    Adjustment Groups ~ refactored a lot of code to support adjustment groups

--- webphoto/webphoto.js
+++ webphoto/webphoto.js
@@ -1,3 +1,5 @@
+let controls = document.getElementById("controlframe")
+
 //describes properties about the filters
 const filters = {
     "contrast":{unit: "%", min: 0, max: 200, val: 100},  
@@ -12,6 +14,7 @@ const filters = {
 
 let renderview = document.getElementById("image")
 
+//the list of adjustment layer blocks
 let order = [];
 
 /**
@@ -19,41 +22,34 @@ Creates the filter render queue
 */
 function render(event){
     let queue = [];
-    for (let filter of order){
-        //should enable brick?
-        if (filter.slider == event.srcElement){
-            filter.enable.checked = true;
-        }
+    //iterate through adjustment groups
+    for (let group of order){
+        //should evaluate this group?
+        if (group.enable.checked){
+            for (let filter of group.adjustments){
+                //should enable adjustment?
+                if (event && filter.slider == event.srcElement){
+                    filter.enable.checked = true;
+                }
 
-        if (filter.enable.checked){
-            let val = filter.slider.value;
-            //add the function, value, and unit to the queue
-            queue.push(`${filter.name}(${val}${filters[filter.name]["unit"]})`);
-        }   
+                if (filter.enable.checked){
+                    let val = filter.slider.value;
+                    //add the function, value, and unit to the queue
+                    queue.push(`${filter.name}(${val}${filters[filter.name]["unit"]})`);
+                }   
+            }
+        }    
     }
     //apply the queue
     renderview.style.filter = queue.join(' ');
 }
 
-//setup combo box for picking adjustments
-{
-    let combo = document.getElementById("filterpicker")
-    let names = Object.keys(filters);
-    for(let name of names){
-        let option = document.createElement('option');
-        option.appendChild(document.createTextNode(name));
-        option.value=name;
-        combo.appendChild(option);
-    }
-}
-
 /**
  * Adds an adjustment
- * @param {} element 
+ * @param {string} filter the name of the adjustment to add
+ * @param {} group the group structure to add
  */
-function addAdjustment(element){
-    let controls = document.getElementById("controlframe")
-    let filter = element.options[element.selectedIndex].text
+function addAdjustment(filter,group){
     let d = filters[filter];
     if (!d.step){
         d.step=1;
@@ -61,18 +57,18 @@ function addAdjustment(element){
     //create the elements that make the adjustment
     let enable = document.createElement('input');
         enable.type = "checkbox";
-    let title = document.createElement('b');
+    let title = document.createElement('i');
         title.innerHTML = filter;
     let slider = document.createElement('input');
         slider.type="range"; slider.style="width:100%";
         slider.min = d.min, slider.max = d.max; slider.value = d.val;
         slider.step = d.step;
-    let separator = document.createElement('hr');
+    let separator = document.createElement('br');
     //append the controls
-    controls.appendChild(enable);
-    controls.appendChild(title);
-    controls.appendChild(slider);
-    controls.appendChild(separator);
+    group.display.appendChild(enable);
+    group.display.appendChild(title);
+    group.display.appendChild(slider);
+    group.display.appendChild(separator);
     //attach events
     slider.oninput = render;
     enable.oninput = render;
@@ -83,16 +79,61 @@ function addAdjustment(element){
         "enable":enable,
         "slider":slider,
     };
-    //for auto-enabling adjustments when a slider is changed
-    slider["renderptr"] = adjustment;
 
     //add slider to render queue
-    order.push(adjustment);
+    group.adjustments.push(adjustment);
 }
 
-function loadurl(){
-    let url = document.getElementById("img-url").value;
-    document.getElementById("image").src=url;
+/**
+ * Create an Adjustment group
+ */
+function addAdjustmentGroup(){
+    //create html elements
+    let root = document.createElement('div');
+    let select = document.createElement('select');
+    let enable = document.createElement('input');
+        enable.type = "checkbox";
+        enable.checked = true;
+    let name = document.createElement('b');
+        name.innerHTML = "Adjustment Group";
+    let addbtn = document.createElement('button');
+        addbtn.innerHTML = "Add Adjustment";
+
+    //setup combo box items
+    let names = Object.keys(filters);
+    for(let name of names){
+        let option = document.createElement('option');
+        option.appendChild(document.createTextNode(name));
+        option.value=name;
+        select.appendChild(option);
+    }
+    root.appendChild(document.createElement('hr'));
+    root.appendChild(enable);
+    root.appendChild(name);
+    root.appendChild(select);
+    root.appendChild(addbtn);
+
+    //create structure
+    let group = {
+        display:root,
+        "name":name,
+        "enable":enable,
+        adjustments:[]
+    };
+    //hook up events
+    addbtn.onclick = function(){
+        let name = select.options[select.selectedIndex].text;
+        addAdjustment(name,group);
+    }
+    enable.oninput = function(){
+        //TODO: dim group if disabled
+        render();
+    };
+    controls.appendChild(root);
+
+    //add group to render list
+    order.push(group);
+
 }
 
 render()
