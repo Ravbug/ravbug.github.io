6cc1d6040fdc23fc0ea846e0b360b58bbdbe45e7
Ravbug <ravbuganimations@gmail.com>
2019-11-30 17:48:39 -0500

    Completed reusable header

--- header.js
+++ header.js
@@ -2,38 +2,30 @@ Object.prototype.insertAfter = function (newNode) { this.parentNode.insertBefore
 
 /** Creates the header for the page
  *  Define the following variables in an earlier `<script>`:
- *  @param {string} page_title the title text for the page
- *  @param {string} page_desc the description for the page
  *  @param {number} stylesheet_depth the distance from the root dir. 0 = at root dir
  * 
- *  Optional parameters
- *  @param {string} page_meta_title The window / embed title. Uses `page_title` if undefined.
- *  @param {string} page_meta_desc The social media embed text. Uses `page_desc` if undefined.
- *  @param {url} page_favicon The icon used in embeds and in the tab. Uses the default R logo if undefined.
  */
 {
     let stylesheetroot = [];
     for (let i = 0; i < stylesheet_depth; i++){
         stylesheetroot.push('../');
     }
+    delete stylesheet_depth;
     stylesheetroot = stylesheetroot.join('');
-    let main = document.createElement('header');
-    main.innerHTML = 
-    `<head>
-        <title>${typeof page_meta_title != "undefined" ? page_meta_title : page_title}</title>
-        <meta name="description" content="${typeof page_meta_desc != "undefined"? page_meta_desc : page_desc}">
+    let head = document.getElementsByTagName('head')[0];
+
+    //get the page title and content as described in the head
+    let page_title = head.getElementsByTagName('title')[0].innerText;
+    let page_desc = head.getElementsByTagName('meta')[0].content;
+
+    //head boilerplate content
+    head.innerHTML += `
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
-        <link id="favicon" rel="icon" href="${typeof page_favicon != "undefined"? page_favicon : "https://avatars2.githubusercontent.com/u/22283943"}" type="image/x-icon">
         <link rel="stylesheet" type="text/css" href="${stylesheetroot}bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="${stylesheetroot}stylesheet.css">
-    </head>
-    
-    `;
-    //replace this script with main
-    document.currentScript.parentElement.insertBefore(main,document.currentScript);
+        <link rel="stylesheet" type="text/css" href="${stylesheetroot}stylesheet.css">`;
 
-    //construct header
+    //construct jumbotron
     let header = document.createElement('div');
     header.className = "jumbotron header";
     header.innerHTML = `
@@ -41,8 +33,8 @@ Object.prototype.insertAfter = function (newNode) { this.parentNode.insertBefore
     <p style="color:white">${page_desc}</p>`;
 
     //add the jumbotron
-    document.currentScript.parentElement.insertAfter(header,document.currentScript);
-
+    p = document.currentScript;
+    document.firstChild.append(header);
     //unload this script
     document.currentScript.remove();
 }
