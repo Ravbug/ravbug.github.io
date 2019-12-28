fd31b112256266ba6ef5d2d0b90022dce770bb31
Ravbug <ravbuganimations@gmail.com>
2019-12-28 14:23:30 -0800

    Dynamic menu buttons homepage.mjs + file added, contains JSON describing the homepage
    header.js
    + load homepage.mjs
    + create menubar buttons with correct CSS classes
    
    index.html
    ~ replaced inline js with script src tag
    
    stylesheet.css
    + classes for menubar buttons
    
    index.html
    ~ fixed inline styles which draw breadcrumb incorrectly

--- header.js
+++ header.js
@@ -5,36 +5,58 @@
  *  @param {number} stylesheet_depth the distance from the root dir. 0 = at root dir
  * 
  */{
-    let body = document.getElementsByTagName("body")[0];
-
     let stylesheetroot = [];
     for (let i = 0; i < stylesheet_depth; i++){
         stylesheetroot.push('../');
     }
+
     delete stylesheet_depth;
     stylesheetroot = stylesheetroot.join('');
-    let head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.onload = function() {
+      generate();
+    };
+    script.src=`${stylesheetroot}homepage.mjs`
+    document.getElementsByTagName('head')[0].appendChild(script);
+    function generate(){
+        let body = document.getElementsByTagName("body")[0];
 
-    //get the page title and content as described in the head
-    let page_title = head.getElementsByTagName('title')[0].innerText;
-    let page_desc = head.getElementsByTagName('meta')[0].content;
+       
+        let head = document.getElementsByTagName('head')[0];
 
-    //head boilerplate content
-    head.innerHTML += `
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="${stylesheetroot}bootstrap-custom.css">
-        <link rel="stylesheet" type="text/css" href="${stylesheetroot}stylesheet.css">`;
+        //get the page title and content as described in the head
+        let page_title = head.getElementsByTagName('title')[0].innerText;
+        let page_desc = head.getElementsByTagName('meta')[0].content;
 
-    //construct jumbotron
-    let header = document.createElement('div');
-    header.className = "jumbotron header";
-    header.innerHTML = `
-    <span style="color:white">${page_title}</span>
-    <p style="color:white">${page_desc}</p>`;
+        //head boilerplate content
+        head.innerHTML += `
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta charset="UTF-8">
+            <link rel="stylesheet" type="text/css" href="${stylesheetroot}bootstrap-custom.css">
+            <link rel="stylesheet" type="text/css" href="${stylesheetroot}stylesheet.css">`;
 
-    //add the jumbotron
-    body.insertBefore(header,body.firstChild);
-    //unload this script
-    document.currentScript.remove();
+        
+        let menus = [`<a class="headerlink btn btn-primary" href="/">Home</a>`];
+        for (let card of data){
+            menus.push(`<a class="headerlink btn btn-primary header-nonessential" href="/#${card.title.toLowerCase().replace(/ /g,'_')}">${card.title}</a>`);
+        }
+        //construct jumbotron
+        let header = document.createElement('div');
+        header.className = "jumbotron header";
+        header.innerHTML = `
+        <span style="color:white">${page_title}</span>
+        <p style="color:white">${page_desc}</p> 
+        ${menus.join(' ')}
+        `;
+
+        delete data;  
+        //add the jumbotron
+        body.insertBefore(header,body.firstChild);
+        //unload this script
+        for (let script of document.scripts){
+            if (script.src.includes("header.js")){
+                script.remove();
+            }
+        }
+    }
 }
