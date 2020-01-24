9714d295062915be6e2c2b6ba51dc775a009cf54
Ravbug <ravbuganimations@gmail.com>
2020-01-24 18:20:55 -0500

    Better code editing index.html + hinting and linting scripts linked ~ cleanup ~ font size default is now 12
    main.js
    + enable linting on change
    + implement intellisense-like autocomplete for HTML
    ~ fix font size spinner
    
    style.css
    ~ unset global font family and font size
    + container default font size

--- codevisualizer/main.js
+++ codevisualizer/main.js
@@ -17,6 +17,7 @@ let editor;
         autohint: true,
         htmlMode:true,
         fixedGutter:true,
+        lint: { lintOnChange: true },
         mode:"htmlmixed",
     }
     //dark mode
@@ -32,6 +33,70 @@ let editor;
 editor.on("change",function(){
     Krender();
 });
+
+//intellisense-like autocomplete: https://stackoverflow.com/questions/13744176/codemirror-autocomplete-after-any-keyup
+editor.on('keyup', (editor,event) => {
+    let ExcludedIntelliSenseTriggerKeys =   {
+        "8": "backspace",
+        "9": "tab",
+        "13": "enter",
+        "16": "shift",
+        "17": "ctrl",
+        "18": "alt",
+        "19": "pause",
+        "20": "capslock",
+        "27": "escape",
+        "33": "pageup",
+        "34": "pagedown",
+        "35": "end",
+        "36": "home",
+        "37": "left",
+        "38": "up",
+        "39": "right",
+        "40": "down",
+        "45": "insert",
+        "46": "delete",
+        "91": "left window key",
+        "92": "right window key",
+        "93": "select",
+        "107": "add",
+        "109": "subtract",
+        "110": "decimal point",
+        "111": "divide",
+        "112": "f1",
+        "113": "f2",
+        "114": "f3",
+        "115": "f4",
+        "116": "f5",
+        "117": "f6",
+        "118": "f7",
+        "119": "f8",
+        "120": "f9",
+        "121": "f10",
+        "122": "f11",
+        "123": "f12",
+        "144": "numlock",
+        "145": "scrolllock",
+        "186": "semicolon",
+        "187": "equalsign",
+        "188": "comma",
+        "189": "dash",
+        "190": "period",
+        "191": "slash",
+        "192": "graveaccent",
+        "220": "backslash",
+        "222": "quote"
+    }
+    var __Cursor = editor.getDoc().getCursor();
+    var __Token = editor.getTokenAt(__Cursor);
+
+    if (!editor.state.completionActive &&
+        !ExcludedIntelliSenseTriggerKeys[(event.keyCode || event.which).toString()] &&
+        (__Token.type == "tag" || __Token.string == " " || __Token.string == "<" || __Token.string == "/"))
+    {
+        CodeMirror.commands.autocomplete(editor, null, { completeSingle: false });
+    }
+  });
 
 editor.setSize(null,null);
 
@@ -180,8 +245,7 @@ slider.oninput = function(){
 
 //adjusts the fontsize and the line #s div if necessary
 function changeFont(newSize){
-  //document.getElementById("lineNums").style.fontSize = newSize + "px";
-  //i.style.fontSize = newSize + "px";
+  document.getElementById("codecontainer").style.fontSize=`${newSize}px`;
 }
 
 //synchronous scrolling between line numbers and code div
