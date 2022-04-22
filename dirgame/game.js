924467cded740e1526b4ef6f2e0abd174d3d5f42
Ravbug <ravbuganimations@gmail.com>
2022-04-21 22:12:12 -0400

    Display an embedded static map at the end of the game

--- dirgame/game.js
+++ dirgame/game.js
@@ -170,6 +170,8 @@ function GeoLocationHandler(geoloc){
         }
         // generate points
         map.innerHTML = "";
+        document.getElementById("latDist").innerHTML = `Scale: ${(distance(bbmin.lat,bbmin.long,bbmax.lat,bbmin.long)*1000).toFixed(1)}m`
+        document.getElementById("longDist").innerHTML = `Scale: ${(distance(bbmin.lat,bbmin.long,bbmin.lat,bbmax.long)*1000).toFixed(1)}m`
         {
             let i = 0;
             for(const point of breadcrumb){
@@ -180,8 +182,11 @@ function GeoLocationHandler(geoloc){
             }
         }
 
+        instructionLabel.innerHTML = `Continue walking in this direction for as long as you can!<br>Try to keep the arrows aligned<br>Distance traveled: ${totalDist().toFixed(1)}m`
+
         // figure out the current heading from the last 3 latlongs
         if (breadcrumb.length > 6){
+            document.getElementById("simpleMap").style.display = ""
             document.getElementById("reloadBtn").style.display = "none";
             document.getElementById("endGameBtn").style.display = "";
 
@@ -265,30 +270,74 @@ function MotionHandler(evt){
     }
 }
 
+// https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+/**
+ * @param {Number} lat1 latitude of point 1
+ * @param {Number} lon1 longitude of point 1
+ * @param {Number} lat2 latitude of point 2
+ * @param {Number} lon2 longitude of point 2
+ * @returns the spherical distance between two points
+ */
+function distance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;    // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+            c(lat1 * p) * c(lat2 * p) * 
+            (1 - c((lon2 - lon1) * p))/2;
+
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
+/**
+ * @returns Distance between endpoints on the breadcrumb
+ */
+function totalDist(){
+    // convert to meters
+    const dist = distance(breadcrumb[0].coords.latitude,breadcrumb[0].coords.longitude,breadcrumb[breadcrumb.length-1].coords.latitude,breadcrumb[breadcrumb.length-1].coords.longitude) * 1000;
+    return dist;
+}
+
+/**
+ * Invoke when the game has ended
+ */
 function gameOver(){
      // game over!
 
      // total score is the distance traveled between the first point and the final point
-
-    // https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
-    function distance(lat1, lon1, lat2, lon2) {
-        var p = 0.017453292519943295;    // Math.PI / 180
-        var c = Math.cos;
-        var a = 0.5 - c((lat2 - lat1) * p)/2 + 
-                c(lat1 * p) * c(lat2 * p) * 
-                (1 - c((lon2 - lon1) * p))/2;
-    
-        return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
-    }
     // convert to meters
-    const dist = distance(breadcrumb[0].coords.latitude,breadcrumb[0].coords.longitude,breadcrumb[breadcrumb.length-1].coords.latitude,breadcrumb[breadcrumb.length-1].coords.longitude) * 1000
-    alert(`You traveled ${dist}m`)
+    const dist = totalDist();
 
-     instructionLabel.innerHTML = "Game Over!"
-     // remove event handlers
-     window.removeEventListener('deviceorientation',OrientationHandler);
-     window.removeEventListener('deviceorientationabsolute',OrientationHandler);
-     navigator.geolocation.clearWatch(watchID);
+    instructionLabel.innerHTML = `Game Over!<br>You traveled: ${totalDist().toFixed(1)}m`
+    document.getElementById("glyphContainer").hidden = true;
+    document.getElementById("reloadBtn").style.display = "";
+    document.getElementById("reloadBtn").innerHTML = "Play Again!"
+    document.getElementById("endGameBtn").style.display = "none";
+    // remove event handlers
+    window.removeEventListener('deviceorientation',OrientationHandler);
+    window.removeEventListener('deviceorientationabsolute',OrientationHandler);
+    navigator.geolocation.clearWatch(watchID);
+
+    // embed the map
+    const queryargs = [];
+    const avg = {lat:0,long:0}
+    let count = 0;
+    for(const point of breadcrumb){
+        avg.lat += point.coords.latitude
+        avg.long += point.coords.longitude
+        count++;
+        let type;
+        if (count == 1){
+            type = "flag"
+        }
+        else{
+            type = "vkgrm"
+        }
+        queryargs.push(`${point.coords.longitude.toFixed(4)},${point.coords.latitude.toFixed(4)},${type}`)
+    }
+    avg.lat /= count;
+    avg.long /= count;
+    const url = `https://static-maps.yandex.ru/1.x/?lang=en-US&ll=${avg.long.toFixed(4)},${avg.lat.toFixed(4)}&z=17&l=map&size=450,450&pt=${queryargs.join('~')}`
+    document.getElementById("mapRegion").innerHTML = `<img src="${url}" style="width:100%;"><br>Disclaimer - this game uses Yandex Maps to display your route because it is the only service that allows you to plot points without paying for a license. This is not support for Russia.`
 }
 
 /**
