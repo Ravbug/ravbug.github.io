c7078dd4439008a0bd934f1115b5ed28e5f6550b
Ravbug <ravbuganimations@gmail.com>
2022-12-29 16:37:13 -0800

    Add more placements

--- delayedload.js
+++ delayedload.js
@@ -1,12 +1,14 @@
 // only load google ads if the the page is not launched locally
 if (window.location.protocol != "file:")
 {
-    let script = document.createElement('script')
-    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5775858651010213"
-    script.async = true
-    script.crossOrigin = "anonymous"
-    document.getElementsByTagName('head')[0].appendChild(script)
+    // automatic
+    // let script = document.createElement('script')
+    // script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5775858651010213"
+    // script.async = true
+    // script.crossOrigin = "anonymous"
+    // document.getElementsByTagName('head')[0].appendChild(script)
 
+    // manual placement
     const displayAdStr = 
     `
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5775858651010213"
