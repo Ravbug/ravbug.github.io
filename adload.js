10c7a468274f5a15436ff830be6125cb81a0a784
Ravbug <ravbuganimations@gmail.com>
2022-12-29 16:19:24 -0800

    experiment

--- adload.js
+++ adload.js
@@ -6,5 +6,25 @@ if (window.location.protocol != "file:")
     script.async = true
     script.crossOrigin = "anonymous"
     document.getElementsByTagName('head')[0].appendChild(script)
+
+    const displayAdStr = 
+    `
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5775858651010213"
+     crossorigin="anonymous"></script>
+<!-- DisplayAd -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5775858651010213"
+     data-ad-slot="6301654661"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    `
+    const allDisplayAds = document.querySelectorAll('displayAd')
+    for(const adElt of allDisplayAds){
+        adElt.innerHTML = displayAdStr
+    }
 }
 document.currentScript.remove()
