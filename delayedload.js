b6bc7f03fc0545ec720a3659dff6e4a362706e83
Ravbug <ravbuganimations@gmail.com>
2022-12-29 21:48:01 -0800

    Try another type

--- delayedload.js
+++ delayedload.js
@@ -19,9 +19,29 @@
      data-ad-format="auto"
      data-full-width-responsive="true"></ins>
     `
+    
     const allDisplayAds = document.querySelectorAll('displayAd')
     for(const adElt of allDisplayAds){
         adElt.innerHTML = displayAdStr
+
+        //script that registers the ad
+        let script = document.createElement('script')
+        script.text = "(adsbygoogle = window.adsbygoogle || []).push({});"
+        adElt.append(script)
+    }
+
+    const multiplexAdStr = 
+    `
+    <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="autorelaxed"
+     data-ad-client="ca-pub-5775858651010213"
+     data-ad-slot="9626710054"></ins>
+    `
+
+    const allMultiplexAds = document.querySelectorAll('multiplexAd')
+    for(const adElt of allMultiplexAds){
+        adElt.innerHTML = multiplexAdStr
 
         //script that registers the ad
         let script = document.createElement('script')
