2911629b83c54737c0162ea9732f6999187dfdcb
Ravbug <ravbug@noreply.users.github.com>
2023-07-03 20:35:53 -0700

    First run of new pack generator

--- mc-googletranslate/generator.js
+++ mc-googletranslate/generator.js
@@ -1,5 +1,5 @@
 const patch_start_version = 16;
-const currentVersion = 19;
+const currentVersion = 20;
 
 // source: https://dev.to/nombrekeff/download-file-from-blob-21ho
 function downloadBlob(blob, name = 'file.txt') {
@@ -41,24 +41,31 @@ async function generatePack(){
     // get the base data
     let full_game_data = await (await fetch(`generator/base_game_${level}.json`)).json()
     let full_realms_data = await (await fetch(`generator/base_realms_${level}.json`)).json()
+    //let full_credits_data = await (await fetch(`generator/base_credits_${level}.json`)).json()
 
     // get the patches
-    for(let i = patch_start_version; i < currentVersion; i++){
+    for(let i = patch_start_version; i <= currentVersion; i++){
         const game_data = await (await fetch(`generator/${i}_game_${level}.json`)).json()
         const realms_data = await (await fetch(`generator/${i}_game_${level}.json`)).json()
         full_game_data = {...full_game_data, ...game_data};
         full_realms_data = {...full_realms_data, ...realms_data};
+        if (i >= 19){
+          //const credits_data = await (await fetch(`generator/${i}_credits_${level}.json`)).json()
+          //full_credits_data = {...full_credits_data, ...credits_data}
+        }
     }
 
-    const splashes = await (await fetch(`generator/${currentVersion}_splashes.txt`)).text()
-    const end = await (await fetch(`generator/${currentVersion}_end.txt`)).text()
-    const postcredits = await (await fetch(`generator/${currentVersion}_postcredits.txt`)).text()
+    const splashes = await (await fetch(`generator/${currentVersion}_splashes_${level}.txt`)).text()
+    const end = await (await fetch(`generator/${currentVersion}_end_${level}.txt`)).text()
+    const postcredits = await (await fetch(`generator/${currentVersion}_postcredits_${level}.txt`)).text()
+
+    const thumbnail = await( await fetch(`generator/icon_${level}.png`)).blob()
 
     const zip = new JSZip();
 
     zip.file("pack.mcmeta",`{
         "pack": {
-           "pack_format": 9,
+           "pack_format": 15,
            "description": "By Ravbug (www.ravbug.com)"
         },
         "language": {
@@ -70,11 +77,15 @@ async function generatePack(){
         }
     }`)
 
+    zip.file("pack.png",thumbnail,{binary:true})
+
     
     // main game strings
     zip.file("assets/minecraft/lang/google_translate.json",JSON.stringify(full_game_data))
     // realms strings
     zip.file("assets/realms/lang/google_translate.json",JSON.stringify(full_realms_data))
+    // (1.19+) new credits format
+    //zip.file("assets/minecraft/texts/credits.json",JSON.stringify(full_credits_data))
 
     // extra strings
     zip.file("assets/minecraft/texts/splashes.txt",splashes)
