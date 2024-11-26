34b315f4c77f9be15333d68bb51767365b5f4030
Ravbug <ravbuganimations@gmail.com>
2024-11-26 12:00:45 -0500

    Update date.js

--- blog/ravengine/date.js
+++ blog/ravengine/date.js
@@ -1,4 +1,4 @@
 {
     let dateobj = new Date(Date.parse(document.currentScript.getAttribute("date")));
-    document.currentScript.outerHTML =`Last Updated: ${dateobj.toLocaleDateString()}<br><p style="color:red">Note: I write these articles as I learn. As such, information present here may be incorrect or out-of-date.</p>`;
+    document.currentScript.outerHTML =`Last Updated: ${dateobj.toLocaleDateString()}<br><p style="color:red">Note: This is NOT engine documentation. I write these articles as I learn. As such, information present here may be incorrect or out-of-date. I'm leaving these pages online for historical purposes only.</p>`;
 }
